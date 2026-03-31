6b7b07e7077690ad49ae2e95741c459c490df9f3
jullionetto-hub <jullionetto@hotmail.com>
2026-03-31 20:28:32 -0300

    feat: adiciona frontend do WMS

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-// ✅ CORRETO
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-    res.send('<h1>🚀 SISTEMA ONLINE!</h1><p>Deploy funcionando!</p>');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor ativo na porta ${PORT}`);
+  console.log(`Servidor ativo na porta ${PORT}`);
 });
