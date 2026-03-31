e247e41e82d404af844155c8f74067f984bf7cb6
jullionetto-hub <jullionetto@hotmail.com>
2026-03-31 18:55:30 -0300

    reset de conexao

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const app = express();
-
-// O Render exige que usemos a porta que ele nos fornece
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('<h1>🚀 Sistema de Pedidos Online Rodando!</h1>');
+    res.send('<h1>🚀 Sistema Online!</h1>');
 });
 
-// Esta parte é OBRIGATÓRIA para o Render não fechar o sistema
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`Servidor ativo na porta ${PORT}`);
 });
